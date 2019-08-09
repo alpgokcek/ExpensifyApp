3b539b299cec9e697ce13d03a719e4b58d437cff
alpgokcek <alp.gokcek1@gmail.com>
2019-08-09 11:46:30 +0300

    removed dummy expense data

--- src/app.js
+++ src/app.js
@@ -11,18 +11,6 @@ import 'normalize.css/normalize.css';
 
 const store = configureStore();
 
-store.dispatch(addExpense({description: 'Water Bill', amount:50,createdAt:23181237}));
-store.dispatch(addExpense({description: 'Gas Bill', amount:150,createdAt:23181234237}));
-store.dispatch(addExpense({description: 'Rent', amount:51230,createdAt:231237}));
-/*
-store.dispatch(setTextFilter('bill'));
-
-setTimeout(()=> {
-    store.dispatch(setTextFilter('water'));
-}, 3000);
-*/
-const state = store.getState();
-const visibleExpenses=getVisibleExpenses(state.expenses, state.filters);
 
 
 const jsx = (
